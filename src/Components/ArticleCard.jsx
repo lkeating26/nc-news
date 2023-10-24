d1b5ba44de0a15643caa602f7c8884ab5ffaa498
lkeating26 <114599878+lkeating26@users.noreply.github.com>
2023-10-24 12:30:52 +0100

    5-view-an-individual-article complete

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export default function ArticleCard({ article }) {
+
   return (
     <div className='article_card'>
         <h3>{article.title}</h3>
         <img className='article_img'src={article.article_img_url} alt="" />
         <p>Author: {article.author}</p>
         <p>Votes: {article.votes}</p>
+        <p>id: {article.article_id}</p>
     </div>
   )
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     
   return (
     <nav className="nav-bar">
-        <p className='nav-btn'>Home</p>
+        <Link to="/" ><p className='nav-btn'>Home</p></Link>
         <p className='nav-btn'>Topics</p>
         <p className='nav-btn'>Create Article</p>
     </nav>
